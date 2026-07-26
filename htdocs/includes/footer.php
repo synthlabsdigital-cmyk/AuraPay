487318b1b2a3d071dafe6586d96b8e75324bae86
BrowniesAreBlack <misteryowsow22@gmail.com>
2026-07-26 16:56:15 +0800

    fix: use BASE_PATH for asset URLs (customer footer)

--- htdocs/includes/footer.php
+++ htdocs/includes/footer.php
@@ -15,6 +15,6 @@ declare(strict_types=1);
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/app.js"></script>
+<script src="<?= BASE_PATH ?>/assets/js/app.js"></script>
 </body>
 </html>
