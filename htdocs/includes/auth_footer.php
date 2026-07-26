56d3c3d71600168d6f0acfb969b899b5761928a9
BrowniesAreBlack <misteryowsow22@gmail.com>
2026-07-26 17:17:45 +0800

    fix: use BASE_PATH for asset URLs (auth footer)

--- htdocs/includes/auth_footer.php
+++ htdocs/includes/auth_footer.php
@@ -11,6 +11,6 @@ $p = product();
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/app.js"></script>
+<script src="<?= BASE_PATH ?>/assets/js/app.js"></script>
 </body>
 </html>
